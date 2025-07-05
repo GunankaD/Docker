dde2c5e566c901cfea7abc902cf51a64bdbbb92c
Gunanka D <158302120+GunankaD@users.noreply.github.com>
2025-07-05 15:53:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ var counter = 0;
 app.get('/', (req, res) => {
     console.log("hi");
     // res.send(`
-    //     <script>console.log(\`hi sent from server, take this and maja ${counter ++}\`) </script>
+    //     <script>console.log(\`hi sent from server ${counter ++}\`) </script>
     //     `);
-    res.json({ message: `Docker is easy lol ${counter ++}`});
+    res.json({ message: `From server. Counter: ${counter ++}`});
 })
 app.listen(PORT, () => {
     console.log(`Express app listening on http://localhost:${PORT}`);
